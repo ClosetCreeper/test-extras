3f2115513581aa7eea81e00356674e257ee26d17
krytec_gaming <95090991+ClosetCreeper@users.noreply.github.com>
2026-02-28 15:49:27 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['pdfjs-dist', 'pdf-lib'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          pdfjs: ['pdfjs-dist'],
+          pdflib: ['pdf-lib'],
+        }
+      }
+    }
+  }
 })
